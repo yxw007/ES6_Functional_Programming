d8cbe88da2a0e73cd42d7e0bd2d188eddd4f9d89
yxw007 <yxw007@users.noreply.github.com>
2022-10-21 15:15:09 +0000

    deploy: 6b5606e48c8968e9aab9375e2e2ff92a6502fabd

---
+++ assets/guide_functor.md.ca82136e.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as l}from"./app.308cdd7c.js";const i=JSON.parse('{"title":"\u51FD\u5B50","description":"","frontmatter":{},"headers":[],"relativePath":"guide/functor.md"}'),p={name:"guide/functor.md"},o=l("",11),e=[o];function t(c,r,F,y,D,A){return a(),n("div",null,e)}const u=s(p,[["render",t]]);export{i as __pageData,u as default};
