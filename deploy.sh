883cccc1aff1a83dfcd26af271d58124249b8c04
Potter <75838259@qq.com>
2022-10-21 14:46:28 +0800

    fix: deploy报错

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git config --global user.email "aa4790139@gmail.com"
+git config --global user.name "Robot"
+
 git init
 git add -A
 git commit -m 'deploy'
@@ -20,4 +23,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yxw007/ES6_Functional_Programming.git master:gh-pages
+git push -f https://github.com/yxw007/ES6_Functional_Programming.git master:gh-pages
